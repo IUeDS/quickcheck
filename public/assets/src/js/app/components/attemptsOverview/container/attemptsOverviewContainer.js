5144e880e253ede1fdfbcc677c9cf6ca0242648e
nicsears <nicsears@iu.edu>
2018-03-21 11:06:35 -0400

    Refactored to incorporate sessionStorage for student results toggle

--- public/assets/src/js/app/components/attemptsOverview/container/attemptsOverviewContainer.js
+++ public/assets/src/js/app/components/attemptsOverview/container/attemptsOverviewContainer.js
@@ -14,23 +14,37 @@ function AttemptsOverviewController(Manage, Utilities) {
 
     //functions
     vm.$onInit = $onInit;
+    vm.getAssessments = getAssessments;
     vm.getStudents = getStudents;
+    vm.isResultsByStudentToggleEnabled = isResultsByStudentToggleEnabled;
     vm.isSubstringNotFound = isSubstringNotFound;
     vm.isViewingByStudent = isViewingByStudent;
+    vm.storageAvailable = storageAvailable;
     vm.toggleStudentResultsView = toggleStudentResultsView;
 
     //variables
     vm.attempts = [];
     vm.currentPage = 'results';
     vm.isResultsByStudentToggled = false;
+    vm.sessionStorageKey = 'iu-eds-quickcheck-student-results-toggle';
     vm.search = {
         'assessmentName': '',
         'studentName': ''
     };
     vm.students = [];
     vm.utils = new Utilities();
-
+    
     function $onInit() {
+        if (vm.isResultsByStudentToggleEnabled()) {
+            vm.isResultsByStudentToggled = 'true';
+            vm.getStudents();
+        }
+        else {
+            vm.getAssessments();
+        }
+    }
+
+    function getAssessments() {
         vm.utils.loadingStarted();
         Manage.getOverview(vm.utils.contextId)
             .then(function (resp) {
@@ -55,6 +69,14 @@ function AttemptsOverviewController(Manage, Utilities) {
         });
     }
 
+    function isResultsByStudentToggleEnabled() {
+        if (!vm.storageAvailable('sessionStorage')) {
+            return false;
+        }
+
+        return JSON.parse(sessionStorage.getItem(vm.sessionStorageKey));
+    }
+
     function isSubstringNotFound(string1, string2) {
         if (string1.toLowerCase().indexOf(string2.toLowerCase()) === -1) {
             return true;
@@ -72,18 +94,51 @@ function AttemptsOverviewController(Manage, Utilities) {
         return false;
     }
 
+    //determine if local storage is available
+    //source: https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+    function storageAvailable(type) {
+        try {
+            var storage = window[type],
+                x = '__storage_test__';
+            storage.setItem(x, x);
+            storage.removeItem(x);
+            return true;
+        } catch (e) {
+            return false;
+        }
+    }
+
     function toggleStudentResultsView() {
         if (vm.isResultsByStudentToggled == 'true') {
             vm.isResultsByStudentToggled = 'false';
+
+            if (vm.storageAvailable('sessionStorage')) {
+                sessionStorage.setItem(vm.sessionStorageKey, false);
+            }
+
+            if (vm.attempts.length) {
+                return;
+            }
+
+            vm.getAssessments();
+
             return;
         }
 
         vm.isResultsByStudentToggled = 'true';
+
+        if (vm.storageAvailable('sessionStorage')) {
+            sessionStorage.setItem(vm.sessionStorageKey, true);
+        }
+
         //only fetch students once, cache in memory afterward
         if (vm.students.length) {
             return;
-        }
+        }        
 
         vm.getStudents();
+
+        return;
+
     }
 }
