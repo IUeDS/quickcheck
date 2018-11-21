f56ff45a81aa26b3bacb5de6c565c7291585e5fc
Mallon, Matthew Grant <mmallon@iu.edu>
2018-11-21 14:05:17 -0500

    Updated timeout feature description in seeder

--- database/seeds/FeatureTableSeeder.php
+++ database/seeds/FeatureTableSeeder.php
@@ -27,7 +27,7 @@ class FeatureTableSeeder extends Seeder {
 
         Feature::create([
             'name' => config('constants.features.ATTEMPT_TIMEOUT'),
-            'description' => 'When the system detects excessive attempts made by a student (more than 2 attempts in 1 minute, where at least one question has been answered per attempt, and on a graded assignment before the due date), the student will receive a 2 minute timeout, to deter random guessing of answers.',
+            'description' => 'When the system detects excessive attempts made by a student (more than 2 attempts in 1 minute, where at least one question has been answered per attempt, and on a graded assignment before the due date), the student will receive a 2 minute timeout, to deter random guessing of answers. An instructor previewing a quick check will not see a timeout message (unless in student view).',
             'default_state' => 'true',
             'admin_only' => 'false'
         ]);
